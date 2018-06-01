ff1409faf158efa2f3306661d837af877ed93f6f
Ao Li <liao@live.dk>
2018-06-01 16:37:45 +0200

    Using new SDK...

---
+++ app/src/main/java/dk/liao/shinemyhue/SetupActivity.java
@@ -0,0 +1,211 @@
+package dk.liao.shinemyhue;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.philips.lighting.hue.sdk.wrapper.HueLog;
+import com.philips.lighting.hue.sdk.wrapper.Persistence;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnection;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnectionCallback;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeConnectionType;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeStateUpdatedCallback;
+import com.philips.lighting.hue.sdk.wrapper.connection.BridgeStateUpdatedEvent;
+import com.philips.lighting.hue.sdk.wrapper.connection.ConnectionEvent;
+import com.philips.lighting.hue.sdk.wrapper.discovery.BridgeDiscovery;
+import com.philips.lighting.hue.sdk.wrapper.discovery.BridgeDiscoveryCallback;
+import com.philips.lighting.hue.sdk.wrapper.discovery.BridgeDiscoveryResult;
+import com.philips.lighting.hue.sdk.wrapper.domain.Bridge;
+import com.philips.lighting.hue.sdk.wrapper.domain.BridgeBuilder;
+import com.philips.lighting.hue.sdk.wrapper.domain.HueError;
+import com.philips.lighting.hue.sdk.wrapper.domain.ReturnCode;
+import com.philips.lighting.hue.sdk.wrapper.knownbridges.KnownBridge;
+import com.philips.lighting.hue.sdk.wrapper.knownbridges.KnownBridges;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class SetupActivity extends AppCompatActivity {
+
+    private static final String TAG = "ShineMyHue.Setup";
+    private Bridge bridge;
+    private BridgeDiscovery bridgeDiscovery;
+
+    static {
+        System.loadLibrary("huesdk");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setup);
+
+        Persistence.setStorageLocation(getFilesDir().getAbsolutePath(),"ShineMyHue");
+        HueLog.setConsoleLogLevel(HueLog.LogLevel.DEBUG, HueLog.LogComponent.ALL);
+
+        if(networkStateIsWifi()){
+            String bridgeIp = getLastUsedBridgeIp();
+            if (bridgeIp == null) {
+                startBridgeDiscovery();
+            } else {
+                connectToBridge(bridgeIp);
+            }
+        }
+        //TODO remote control
+
+    }
+    private boolean networkStateIsWifi(){
+        //TODO checking outer cases needed (conmanager failed or something) dunno why sometimes returns null
+        ConnectivityManager connManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        if(connManager!= null)
+        {
+            NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                return networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+            }
+        }
+        return false;
+    }
+
+
+    private void connectToBridge(String bridgeIp) {
+        stopBridgeDiscovery();
+        disconnectFromBridge();
+
+        bridge = new BridgeBuilder("app name", "device name")
+                .setIpAddress(bridgeIp)
+                .setConnectionType(BridgeConnectionType.LOCAL)
+                .setBridgeConnectionCallback(bridgeConnectionCallback)
+                .addBridgeStateUpdatedCallback(bridgeStateUpdatedCallback)
+                .build();
+
+        bridge.connect();
+
+        //bridgeIpTextView.setText("Bridge IP: " + bridgeIp);
+        //updateUI(UIState.Connecting, "Connecting to bridge...");
+    }
+    private BridgeStateUpdatedCallback bridgeStateUpdatedCallback = new BridgeStateUpdatedCallback() {
+        @Override
+        public void onBridgeStateUpdated(Bridge bridge, BridgeStateUpdatedEvent bridgeStateUpdatedEvent) {
+            Log.i(TAG, "Bridge state updated event: " + bridgeStateUpdatedEvent);
+
+            switch (bridgeStateUpdatedEvent) {
+                case INITIALIZED:
+                    // The bridge state was fully initialized for the first time.
+                    // It is now safe to perform operations on the bridge state.
+                    //updateUI(UIState.Connected, "Connected!");
+                    break;
+
+                case LIGHTS_AND_GROUPS:
+                    // At least one light was updated.
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    };
+    private String getLastUsedBridgeIp() {
+        List<KnownBridge> bridges = KnownBridges.getAll();
+
+        if (bridges.isEmpty()) {
+            return null;
+        }
+
+        return Collections.max(bridges, new Comparator<KnownBridge>() {
+            @Override
+            public int compare(KnownBridge a, KnownBridge b) {
+                return a.getLastConnected().compareTo(b.getLastConnected());
+            }
+        }).getIpAddress();
+    }
+
+    private void startBridgeDiscovery() {
+        disconnectFromBridge();
+        bridgeDiscovery = new BridgeDiscovery();
+        // ALL Include [UPNP, IPSCAN, NUPNP] but in some nets UPNP and NUPNP is not working properly
+        bridgeDiscovery.search(BridgeDiscovery.BridgeDiscoveryOption.ALL, bridgeDiscoveryCallback);
+        //updateUI(UIState.BridgeDiscoveryRunning, "Scanning the network for hue bridges...");
+    }
+    private BridgeDiscoveryCallback bridgeDiscoveryCallback = new BridgeDiscoveryCallback() {
+        @Override
+        public void onFinished(final List<BridgeDiscoveryResult> results, final ReturnCode returnCode) {
+            // Set to null to prevent stopBridgeDiscovery from stopping it
+            bridgeDiscovery = null;
+/*
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if (returnCode == ReturnCode.SUCCESS) {
+                        bridgeDiscoveryListView.setAdapter(new BridgeDiscoveryResultAdapter(getApplicationContext(), results));
+                        bridgeDiscoveryResults = results;
+
+                        updateUI(UIState.BridgeDiscoveryResults, "Found " + results.size() + " bridge(s) in the network.");
+                    } else if (returnCode == ReturnCode.STOPPED) {
+                        Log.i(TAG, "Bridge discovery stopped.");
+                    } else {
+                        updateUI(UIState.Idle, "Error doing bridge discovery: " + returnCode);
+                    }
+                }
+            });*/
+        }
+    };
+    private void disconnectFromBridge() {
+        if (bridge != null) {
+            bridge.disconnect();
+            bridge = null;
+        }
+    }
+    private void stopBridgeDiscovery() {
+        if (bridgeDiscovery != null) {
+            bridgeDiscovery.stop();
+            bridgeDiscovery = null;
+        }
+    }
+    private BridgeConnectionCallback bridgeConnectionCallback = new BridgeConnectionCallback() {
+        @Override
+        public void onConnectionEvent(BridgeConnection bridgeConnection, ConnectionEvent connectionEvent) {
+            Log.i(TAG, "Connection event: " + connectionEvent);
+
+            switch (connectionEvent) {
+                case LINK_BUTTON_NOT_PRESSED:
+                    //updateUI(UIState.Pushlinking, "Press the link button to authenticate.");
+                    break;
+
+                case COULD_NOT_CONNECT:
+                    //updateUI(UIState.Connecting, "Could not connect.");
+                    break;
+
+                case CONNECTION_LOST:
+                    //updateUI(UIState.Connecting, "Connection lost. Attempting to reconnect.");
+                    break;
+
+                case CONNECTION_RESTORED:
+                    //updateUI(UIState.Connected, "Connection restored.");
+                    break;
+
+                case DISCONNECTED:
+                    // User-initiated disconnection.
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onConnectionError(BridgeConnection bridgeConnection, List<HueError> list) {
+            for (HueError error : list) {
+                Log.e(TAG, "Connection error: " + error.toString());
+            }
+        }
+
+    };
+}
+
+
+
